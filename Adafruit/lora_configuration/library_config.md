671c5356315a0af0803d7771e96b4e78ce3a331d
jb2328 <56542918+jb2328@users.noreply.github.com>
2020-03-29 20:58:48 +0100

    Added Adafruit TTN code
    fully connected to TTN

---
+++ Adafruit/lora_configuration/library_config.md
@@ -0,0 +1,2 @@
+must change EU freq in ..\Arduino\libraries\MCCI_LoRaWAN_LMIC_library\project_config\lmic_project_config.h
+otherwise will not work
---
+++ Adafruit/lora_configuration/ttn-otaa-feather/ttn-otaa-feather.ino
@@ -0,0 +1,255 @@
+/*******************************************************************************
+   The Things Network - Sensor Data Example
+
+   Example of sending a valid LoRaWAN packet with DHT22 temperature and
+   humidity data to The Things Networ using a Feather M0 LoRa.
+
+   Learn Guide: https://learn.adafruit.com/the-things-network-for-feather
+
+   Copyright (c) 2015 Thomas Telkamp and Matthijs Kooijman
+   Copyright (c) 2018 Terry Moore, MCCI
+   Copyright (c) 2018 Brent Rubell, Adafruit Industries
+
+   Permission is hereby granted, free of charge, to anyone
+   obtaining a copy of this document and accompanying files,
+   to do whatever they want with them without any restriction,
+   including, but not limited to, copying, modification and redistribution.
+   NO WARRANTY OF ANY KIND IS PROVIDED.
+ *******************************************************************************/
+#include <lmic.h>
+#include <hal/hal.h>
+#include <SPI.h>
+
+
+#define CFG_eu868 1
+#define CFG_sx1276_radio 1
+
+
+
+static uint8_t mydata[] = "Hello, world!";
+
+// This EUI must be in little-endian format, so least-significant-byte
+// first. When copying an EUI from ttnctl output, this means to reverse
+// the bytes. For TTN issued EUIs the last bytes should be 0xD5, 0xB3,
+// 0x70.
+static const u1_t PROGMEM APPEUI[8] = {  0x65, 0xAB, 0x01, 0xD0, 0x7E, 0xD5, 0xB3, 0x70  };
+void os_getArtEui (u1_t* buf) {
+  memcpy_P(buf, APPEUI, 8);
+}
+
+// This should also be in little endian format, see above.
+static const u1_t PROGMEM DEVEUI[8] = { 0xED, 0x57, 0x11, 0x00, 0x00, 0xB6, 0x76, 0x98 };
+void os_getDevEui (u1_t* buf) {
+  memcpy_P(buf, DEVEUI, 8);
+}
+
+// This key should be in big endian format (or, since it is not really a
+// number but a block of memory, endianness does not really apply). In
+// practice, a key taken from the TTN console can be copied as-is.
+static const u1_t PROGMEM APPKEY[16] = { 0xA4, 0x39, 0x1D, 0x44, 0xBA, 0xBC, 0xE8, 0xB6, 0x97, 0x60, 0x1C, 0xE0, 0x9E, 0x5C, 0x93, 0x09 };
+void os_getDevKey (u1_t* buf) {
+  memcpy_P(buf, APPKEY, 16);
+}
+
+// payload to send to TTN gateway
+static uint8_t payload[5];
+static osjob_t sendjob;
+
+// Schedule TX every this many seconds (might become longer due to duty
+// cycle limitations).
+const unsigned TX_INTERVAL = 5;
+
+// Pin mapping for Adafruit Feather M0 LoRa
+const lmic_pinmap lmic_pins = {
+  .nss = 8,
+  .rxtx = LMIC_UNUSED_PIN,
+  .rst = 4,
+  .dio = {3, 6, LMIC_UNUSED_PIN},
+  .rxtx_rx_active = 0,
+  .rssi_cal = 8,              // LBT cal for the Adafruit Feather M0 LoRa, in dB
+  .spi_freq = 8000000,
+};
+
+
+// init. DHT
+//DHT dht(DHTPIN, DHTTYPE);
+
+void printHex2(unsigned v) {
+  v &= 0xff;
+  if (v < 16)
+    Serial.print('0');
+  Serial.print(v, HEX);
+}
+
+void onEvent (ev_t ev) {
+  Serial.print(os_getTime());
+  Serial.print(": ");
+  switch (ev) {
+    case EV_SCAN_TIMEOUT:
+      Serial.println(F("EV_SCAN_TIMEOUT"));
+      break;
+    case EV_BEACON_FOUND:
+      Serial.println(F("EV_BEACON_FOUND"));
+      break;
+    case EV_BEACON_MISSED:
+      Serial.println(F("EV_BEACON_MISSED"));
+      break;
+    case EV_BEACON_TRACKED:
+      Serial.println(F("EV_BEACON_TRACKED"));
+      break;
+    case EV_JOINING:
+      Serial.println(F("EV_JOINING"));
+      break;
+    case EV_JOINED:
+      Serial.println(F("EV_JOINED"));
+      {
+        u4_t netid = 0;
+        devaddr_t devaddr = 0;
+        u1_t nwkKey[16];
+        u1_t artKey[16];
+        LMIC_getSessionKeys(&netid, &devaddr, nwkKey, artKey);
+        Serial.print("netid: ");
+        Serial.println(netid, DEC);
+        Serial.print("devaddr: ");
+        Serial.println(devaddr, HEX);
+        Serial.print("AppSKey: ");
+        for (size_t i = 0; i < sizeof(artKey); ++i) {
+          if (i != 0)
+            Serial.print("-");
+          printHex2(artKey[i]);
+        }
+        Serial.println("");
+        Serial.print("NwkSKey: ");
+        for (size_t i = 0; i < sizeof(nwkKey); ++i) {
+          if (i != 0)
+            Serial.print("-");
+          printHex2(nwkKey[i]);
+        }
+        Serial.println();
+      }
+      // Disable link check validation (automatically enabled
+      // during join, but because slow data rates change max TX
+      // size, we don't use it in this example.
+      LMIC_setLinkCheckMode(0);
+      break;
+    /*
+      || This event is defined but not used in the code. No
+      || point in wasting codespace on it.
+      ||
+      || case EV_RFU1:
+      ||     Serial.println(F("EV_RFU1"));
+      ||     break;
+    */
+    case EV_JOIN_FAILED:
+      Serial.println(F("EV_JOIN_FAILED"));
+      break;
+    case EV_REJOIN_FAILED:
+      Serial.println(F("EV_REJOIN_FAILED"));
+      break;
+      break;
+    case EV_TXCOMPLETE:
+      Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+      if (LMIC.txrxFlags & TXRX_ACK)
+        Serial.println(F("Received ack"));
+      if (LMIC.dataLen) {
+        Serial.println(F("Received "));
+        Serial.println(LMIC.dataLen);
+        Serial.println(F(" bytes of payload"));
+      }
+      // Schedule next transmission
+      os_setTimedCallback(&sendjob, os_getTime() + sec2osticks(TX_INTERVAL), do_send);
+      break;
+    case EV_LOST_TSYNC:
+      Serial.println(F("EV_LOST_TSYNC"));
+      break;
+    case EV_RESET:
+      Serial.println(F("EV_RESET"));
+      break;
+    case EV_RXCOMPLETE:
+      // data received in ping slot
+      Serial.println(F("EV_RXCOMPLETE"));
+      break;
+    case EV_LINK_DEAD:
+      Serial.println(F("EV_LINK_DEAD"));
+      break;
+    case EV_LINK_ALIVE:
+      Serial.println(F("EV_LINK_ALIVE"));
+      break;
+    /*
+      || This event is defined but not used in the code. No
+      || point in wasting codespace on it.
+      ||
+      || case EV_SCAN_FOUND:
+      ||    Serial.println(F("EV_SCAN_FOUND"));
+      ||    break;
+    */
+    case EV_TXSTART:
+      Serial.println(F("EV_TXSTART"));
+      break;
+    case EV_TXCANCELED:
+      Serial.println(F("EV_TXCANCELED"));
+      break;
+    case EV_RXSTART:
+      /* do not print anything -- it wrecks timing */
+      break;
+    case EV_JOIN_TXCOMPLETE:
+      Serial.println(F("EV_JOIN_TXCOMPLETE: no JoinAccept"));
+      break;
+
+    default:
+      Serial.print(F("Unknown event: "));
+      Serial.println((unsigned) ev);
+      break;
+  }
+}
+
+void do_send(osjob_t* j) {
+  // Check if there is not a current TX/RX job running
+  if (LMIC.opmode & OP_TXRXPEND) {
+    Serial.println(F("OP_TXRXPEND, not sending"));
+  } else {
+    // Prepare upstream data transmission at the next possible time.
+    LMIC_setTxData2(1, mydata, sizeof(mydata) - 1, 0);
+    Serial.println(F("Packet queued"));
+  }
+  // Next TX is scheduled after TX_COMPLETE event.
+}
+void setup() {
+  delay(2000);
+  while (! Serial);
+  Serial.begin(9600);
+  Serial.println(F("Starting"));
+
+  // dht.begin();
+
+  // LMIC init
+  os_init();
+  // Reset the MAC state. Session and pending data transfers will be discarded.
+  LMIC_reset();
+  // Disable link-check mode and ADR, because ADR tends to complicate testing.
+  LMIC_setLinkCheckMode(0);
+  // Set the data rate to Spreading Factor 7.  This is the fastest supported rate for 125 kHz channels, and it
+  // minimizes air time and battery power. Set the transmission power to 14 dBi (25 mW).
+#if defined(CFG_eu868)
+  Serial.println("European Channels");
+  for (int i = 1; i <= 8; i++) LMIC_disableChannel(i);
+#endif
+
+  LMIC_setDrTxpow(DR_SF7, 14);
+  // in the US, with TTN, it saves join time if we start on subband 1 (channels 8-15). This will
+  // get overridden after the join by parameters from the network. If working with other
+  // networks or in other regions, this will need to be changed.
+ // LMIC_selectSubBand(1);
+
+  // Start job (sending automatically starts OTAA too)
+  do_send(&sendjob);
+}
+
+void loop() {
+  // we call the LMIC's runloop processor. This will cause things to happen based on events and time. One
+  // of the things that will happen is callbacks for transmission complete or received messages. We also
+  // use this loop to queue periodic data transmissions.  You can put other things here in the `loop()` routine,
+  // but beware that LoRaWAN timing is pretty tight, so if you do more than a few milliseconds of work, you
+  // will want to call `os_runloop_once()` every so often, to keep the radio running.
+  os_runloop_once();
+}
